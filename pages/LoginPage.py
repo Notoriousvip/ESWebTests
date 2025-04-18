8d3a399c381d045bb43918d29b428fe0306ad94b
eva <primerxo@mail.ru>
2025-04-18 19:45:57 +0300

    Урок 3. Автотесты с использованием паттерна PageObject

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -10,11 +10,33 @@ class LoginPageLocators:
     PASSWORD_FIELD = (By.ID, 'field_password')
     LOGIN_BY_QRCODE_BUTTON = (By.XPATH, '//*[@data-l="t,get_qr"]')
     CANNOT_LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,restore"]')
-    REGISTER_BUTTON = (By.XPATH, '//*[@data-l="t,register"]')
+    REGISTER_BUTTON = (By.XPATH, '//div[@class="external-oauth-login-footer"]/a[@data-l="t,register"]')
     LOGIN_THROUGH_VK = (By.XPATH, '//*[@class="i ic social-icon __s __vk_id"]')
     LOGIN_THROUGH_MAILRU = (By.XPATH, '//*[@class="i ic social-icon __s __mailru"]')
     LOGIN_THROUGH_YANDEX = (By.XPATH, '//*[@class="i ic social-icon __s __yandex"]')
+    ERROR_TEXT = (By.XPATH, '//*[@class="input-e login_error"]')
 
 
-class LoginPageHelper(BasePage)
-    pass
+class LoginPageHelper(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.UPPER_ENTRANCE_BUTTON)
+        self.find_element(LoginPageLocators.UPPER_QRCODE_BUTTON)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BY_QRCODE_BUTTON)
+        self.find_element(LoginPageLocators.CANNOT_LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.REGISTER_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_THROUGH_VK)
+        self.find_element(LoginPageLocators.LOGIN_THROUGH_MAILRU)
+        self.find_element(LoginPageLocators.LOGIN_THROUGH_YANDEX)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+
+    def get_error_text(self):
+        return self.find_element(LoginPageLocators.ERROR_TEXT).text
